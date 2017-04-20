5ad94ab59c2dfe1394993931cca5d3ebd5650a63
pegasusCK <cky1026@gmail.com>
2017-04-20 15:11:19 -0700

    Lesson 62 Designing the UI

---
+++ PartyRock/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by Chihkao Yu on 4/20/17.
+//  Copyright © 2017 Chihkao Yu. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
