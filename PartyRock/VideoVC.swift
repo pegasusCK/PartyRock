e974997f6b13abbe77bd1f5d44146c5ac0110ee1
pegasusCK <cky1026@gmail.com>
2017-04-25 14:20:30 -0700

    Lesson 65 Showing Youtube Videos

--- PartyRock/VideoVC.swift
+++ PartyRock/VideoVC.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class VideoVC: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        titleLbl.text = _partyRock.videoTitle
+        webView.loadHTMLString(_partyRock.videoURL, baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
